dbe8d01cbd76e5dee9aacc3289a8a148650da932
André Luiz Amorim de Souza <andre_luiz_amorim@estudante.sc.senai.br>
2023-10-16 16:39:00 -0300

    Adicionando rota post para criação de usuario

--- src/routes/usuario.routes.js
+++ src/routes/usuario.routes.js
@@ -2,3 +2,5 @@ const { Router } = require("express");
 const { criarUsuario } = require("../controllers/usuarios/criarUsuario");
 
 const usuarioRoutes = new Router();
+
+usuarioRoutes.post("/api/usuarios", criarUsuario);
