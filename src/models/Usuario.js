5f674091a1dc55a2d4750bf34ff69717007af465
André Luiz Amorim de Souza <andre_luiz_amorim@estudante.sc.senai.br>
2023-10-16 16:35:02 -0300

    Criado arquivo model de Usuario

---
+++ src/models/Usuario.js
@@ -0,0 +1,46 @@
+const { INTEGER, STRING, ENUM, BOOLEAN } = require("sequelize");
+const { sequelize } = require("../database/conexao");
+
+const Usuario = sequelize.define("usuarios", {
+  usuarioId: {
+    type: INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+  },
+  nomeCompleto: {
+    type: STRING(64),
+    allowNull: false,
+  },
+  genero: {
+    type: ENUM("FEMININO", "MASCULINO", "OUTRO"),
+    allowNull: false,
+  },
+  cpf: {
+    type: STRING(11),
+    allowNull: false,
+  },
+  telefone: {
+    type: STRING(12),
+    allowNull: false,
+  },
+  email: {
+    type: STRING,
+    allowNull: false,
+  },
+  senha: {
+    type: STRING,
+    allowNull: false,
+  },
+  tipo: {
+    type: ENUM("ADMINISTRADOR", "MEDICO", "ENFERMEIRO"),
+    allowNull: false,
+  },
+  statusSistema: {
+    type: BOOLEAN,
+    allowNull: false,
+    defaultValue: true,
+  },
+});
+
+module.exports = { Usuario };
