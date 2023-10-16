3f8f0bfe2009320dc2ecb92c84dd8005a75b0495
André Luiz Amorim de Souza <andre_luiz_amorim@estudante.sc.senai.br>
2023-10-16 16:31:55 -0300

    Alterado conexão ao banco para arquivo separado

---
+++ src/database/conexao.js
@@ -0,0 +1,18 @@
+const { Sequelize } = require("sequelize");
+const DB_CONFIG = require("./config/database");
+
+const sequelize = new Sequelize(DB_CONFIG);
+
+const connect = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Conexão com banco de dados estabelecida com sucesso");
+  } catch (err) {
+    console.log("Problema de conexão com banco de dados", err);
+  }
+};
+
+module.exports = {
+  sequelize,
+  connect,
+};
