a1f1a83273e0fe3e15599ffcd040cdf3d6084974
André Luiz Amorim de Souza <andre_luiz_amorim@estudante.sc.senai.br>
2023-10-19 17:48:50 -0300

    Adicionado try catch e ajustes no middleware validarToken

--- src/middlewares/validarToken.js
+++ src/middlewares/validarToken.js
@@ -1,32 +1,31 @@
 const { verify } = require("jsonwebtoken");
 
 const validarToken = (request, response, next) => {
-  const { authorization } = request.headers;
+  try {
+    const { authorization } = request.headers;
 
-  const token = authorization?.replace("Bearer", "").trim();
+    const token = authorization?.replace(/Bearer\s*/, "").trim();
 
-  if (!token) {
-    return response.status(401).json({
-      message: "É necessário estar autenticado para acessar esse recurso",
-    });
-  }
+    if (!token) {
+      return response.status(401).json({
+        message: "É necessário estar autenticado para acessar esse recurso",
+      });
+    }
 
-  verify(token, process.env.APP_SECRET, (err, decoded) => {
-    if (err) {
-      if (err.name === "TokenExpiredError") {
-        return response.status(401).json({ message: "Token expirado" });
-      } else if (err.name === "JsonWebTokenError") {
-        return response.status(401).json({ message: "Token inválido" });
-      } else {
+    verify(token, process.env.APP_SECRET, (err, decoded) => {
+      if (err) {
+        console.error("Erro ao verificar o token:", err);
         return response
-          .status(500)
-          .json({ message: "Erro interno do servidor" });
+          .status(401)
+          .json({ message: "Token inválido ou expirado" });
       }
-    } else {
       request.usuario = decoded;
       next();
-    }
-  });
+    });
+  } catch (error) {
+    console.error("Erro ao verificar o token:", err);
+    return response.status(500).json({ message: "Erro interno do servidor" });
+  }
 };
 
 module.exports = { validarToken };
