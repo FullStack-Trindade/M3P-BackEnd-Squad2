ebce69a0f371ef02051864ac6a301b75ed0d90d6
Alexandre Mariano <86934710+devmariano@users.noreply.github.com>
2023-10-26 17:56:58 -0300

    adicionado validateToken a rota de listagem de log

--- src/routes/log.routes.js
+++ src/routes/log.routes.js
@@ -1,8 +1,9 @@
 const { Router } = require('express');
 const { listarLogs } = require('../controllers/logs/log.controller');
+const { validarToken } = require("../middlewares/validarToken");
 
 const logRoutes = new Router();
 
-logRoutes.get('/logs', listarLogs);
+logRoutes.get('/logs', validarToken, listarLogs);
 
 module.exports = { logRoutes };
